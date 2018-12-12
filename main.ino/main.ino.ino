781e5b5c93214c8a2579a1b9acf783faa138f7d8
Thomas Chaplin <thomaschaplin@outlook.com>
2018-12-12 23:07:51 +0000

    Initial commit

---
+++ main.ino/main.ino.ino
@@ -0,0 +1,8 @@
+#include "FastLED.h"
+CRGB leds[60];
+void setup() { FastLED.addLeds<NEOPIXEL, 6>(leds, 60); }
+void loop() { 
+  static uint8_t hue = 0;
+  FastLED.showColor(CHSV(hue++, 255, 255)); 
+  delay(10);
+}
